af1bcf1f91747622e1e2d042a7f5355da6eb24d7
RogerQi <17267101+RogerQi@users.noreply.github.com>
2022-09-24 04:13:34 -0500

    use a unified argparser across different main implementations

--- validate_only_main.py
+++ validate_only_main.py
@@ -34,73 +34,7 @@ torch.backends.cudnn.benchmark = True
 
 import torchvision as tv
 import torchvision.transforms.functional as tr_F
-
-def get_argparser():
-    parser = argparse.ArgumentParser()
-
-    # Datset Options
-    parser.add_argument("--data_root", type=str, default='/data/DB/VOC2012',
-                        help="path to Dataset")
-    parser.add_argument("--dataset", type=str, default='voc', choices=['voc', 'ade'], help='Name of dataset')
-    parser.add_argument("--num_classes", type=int, default=None, help="num classes (default: None)")
-
-    # Deeplab Options
-    parser.add_argument("--model", type=str, default='deeplabv3_resnet101',
-                        choices=['deeplabv3_resnet50',  'deeplabv3plus_resnet50',
-                                 'deeplabv3_resnet101', 'deeplabv3plus_resnet101',
-                                 'deeplabv3_mobilenet', 'deeplabv3plus_mobilenet'], help='model name')
-    parser.add_argument("--separable_conv", action='store_true', default=False,
-                        help="apply separable conv to decoder and aspp")
-    parser.add_argument("--output_stride", type=int, default=16, choices=[8, 16])
-
-    # Train Options
-    parser.add_argument("--amp", action='store_true', default=False)
-    parser.add_argument("--test_only", action='store_true', default=False)
-    parser.add_argument("--train_epoch", type=int, default=50,
-                        help="epoch number")
-    parser.add_argument("--curr_itrs", type=int, default=0)
-    parser.add_argument("--lr", type=float, default=0.01,
-                        help="learning rate (default: 0.01)")
-    parser.add_argument("--lr_policy", type=str, default='warm_poly', choices=['poly', 'step', 'warm_poly'],
-                        help="learning rate scheduler")
-    parser.add_argument("--step_size", type=int, default=10000)
-    parser.add_argument("--crop_val", action='store_true', default=False,
-                        help='crop validation (default: False)')
-    parser.add_argument("--batch_size", type=int, default=32,
-                        help='batch size (default: 16)')
-    parser.add_argument("--val_batch_size", type=int, default=1,
-                        help='batch size for validation (default: 4)')
-    parser.add_argument("--crop_size", type=int, default=513)
-    
-    parser.add_argument("--ckpt", default=None, type=str,
-                        help="restore from checkpoint")
-
-    parser.add_argument("--loss_type", type=str, default='bce_loss',
-                        choices=['ce_loss', 'focal_loss', 'bce_loss'], help="loss type")
-    parser.add_argument("--gpu_id", type=str, default='0',
-                        help="GPU ID")
-    parser.add_argument("--weight_decay", type=float, default=1e-4,
-                        help='weight decay (default: 1e-4)')
-    parser.add_argument("--random_seed", type=int, default=1,
-                        help="random seed (default: 1)")
-    parser.add_argument("--print_interval", type=int, default=10,
-                        help="print interval of loss (default: 10)")
-    parser.add_argument("--val_interval", type=int, default=100,
-                        help="epoch interval for eval (default: 100)")
-    
-    # CIL options
-    parser.add_argument("--pseudo", action='store_true', help="enable pseudo-labeling")
-    parser.add_argument("--pseudo_thresh", type=float, default=0.7, help="confidence threshold for pseudo-labeling")
-    parser.add_argument("--task", type=str, default='15-1', help="cil task")
-    parser.add_argument("--curr_step", type=int, default=0)
-    parser.add_argument("--overlap", action='store_true', help="overlap setup (True), disjoint setup (False)")
-    parser.add_argument("--mem_size", type=int, default=0, help="size of examplar memory")
-    parser.add_argument("--freeze", action='store_true', help="enable network freezing")
-    parser.add_argument("--bn_freeze", action='store_true', help="enable batchnorm freezing")
-    parser.add_argument("--w_transfer", action='store_true', help="enable weight transfer")
-    parser.add_argument("--unknown", action='store_true', help="enable unknown modeling")
-    
-    return parser
+import argparser
 
 def get_dataset(opts):
     """ Dataset And Augmentation
@@ -232,7 +166,7 @@ def main(opts):
 
 if __name__ == '__main__':
             
-    opts = get_argparser().parse_args()
+    opts = argparser.get_argparser().parse_args()
         
     opts.curr_step = 5 # STEP to test
     main(opts)
